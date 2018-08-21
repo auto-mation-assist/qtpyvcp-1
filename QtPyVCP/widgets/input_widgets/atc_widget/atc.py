0f3275bb5b3552f72c5261d78f519238c3dc7d77
Kurt Jacobson <kcjengr@gmail.com>
2018-08-21 14:08:35 -0400

    prevent designer segfault by not loading QML if no parent

--- QtPyVCP/widgets/input_widgets/atc_widget/atc.py
+++ QtPyVCP/widgets/input_widgets/atc_widget/atc.py
@@ -30,8 +30,10 @@ def _initComp():
 class DynATC(QQuickWidget):
 
     def __init__(self, parent=None):
-
         super(DynATC, self).__init__(parent)
+
+        if parent is None:
+            return
 
         self.engine().rootContext().setContextProperty("atc_spiner", self)
         url = QUrl.fromLocalFile(os.path.join(WIDGET_PATH, "atc.qml"))
